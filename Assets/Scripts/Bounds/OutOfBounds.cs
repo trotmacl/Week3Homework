16089279044c02e4ade5160aef8af44def64fa94
trotmacl <64763657+trotmacl@users.noreply.github.com>
2021-09-19 00:11:50 -0400

    Add files via upload

--- Assets/Scripts/Bounds/OutOfBounds.cs
+++ Assets/Scripts/Bounds/OutOfBounds.cs
@@ -2,9 +2,16 @@
 
 public class OutOfBounds : MonoBehaviour
 {
+    [SerializeField]
+    private GameManager gameManager = null;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //TODO: Implement functionality to reset the game somehow.
         // Resetting the game includes destroying the out of bounds ball and creating a new one ready to be launched from the paddle
+        if (collision.CompareTag("Ball")) {
+            Destroy(collision.gameObject);
+            gameManager.paddle.SpawnBall();
+        }
     }
 }
